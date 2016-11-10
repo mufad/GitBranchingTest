7e3fd7343ebda1d0d2a724b803d5545a4c8483aa
Monwar Jahan <mmufad@gmail.com>
2016-11-10 18:19:06 +0600

    Initial commit

---
+++ app/src/main/java/com/droiddigger/gitbranchingtest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.droiddigger.gitbranchingtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void master(View view) {
+        Toast.makeText(MainActivity.this, "I'mm your father Luke",Toast.LENGTH_SHORT).show();
+    }
+}
